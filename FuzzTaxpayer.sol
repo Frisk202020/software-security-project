8b57132c7eba74646af545dcc583ebcbcc7773d2
c.heme <>
2025-12-24 14:34:30 +0100

    is_contract

--- FuzzTaxpayer.sol
+++ FuzzTaxpayer.sol
@@ -6,7 +6,13 @@ import "./Taxpayer.sol";
 contract FuzzTaxpayer is Taxpayer {
     constructor() Taxpayer(address(1), address(2)) {}
 
+    // If person is married to someeone, that someone mustn't be NULL address
     function echidna_check_spouse_exists() public view returns (bool) {
         return (!isMarried || isMarried && spouse != address(0)); 
+    }
+
+    // isContract should always be true
+    function echidna_is_contract() public view returns (bool) {
+        return (iscontract);
     }
 }
